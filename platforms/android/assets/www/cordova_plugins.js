e86a5511686a1ba0665b594d50e43ef646dc2876
jgeng <jgeng@adobe.com>
2015-02-23 16:09:57 -0700

    Init

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/ADBMobile/sdks/Cordova/ADBMobile/Shared/ADB_Helper.js",
+        "id": "ADBMobile.ADB",
+        "clobbers": [
+            "ADB"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "ADBMobile": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
